1b20edb8d1ec35782330557fc4942969a753a463
karukaaa <arukhanym.zhaidary@gmail.com>
2024-04-20 20:48:55 +0500

    models

--- wardrobe_web_api/items/admin.py
+++ wardrobe_web_api/items/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Item
+from . import models
 
-admin.site.register(Item)
+admin.site.register(models.Item)
+admin.site.register(models.Category)
--- wardrobe_web_api/items/models.py
+++ wardrobe_web_api/items/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+    
+class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+
+    def to_json(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "price": self.price
+        }
 
 class Item(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     season = models.CharField(max_length=100)
@@ -8,6 +23,7 @@ class Item(models.Model):
     amount = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def to_json(self):
         return{
